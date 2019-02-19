4b2cb16cce128a47041f3245086dea20b6003aa6
Thomas Marstrander <marstranderthomas@gmail.com>
2019-02-19 16:30:42 +0100

    Update env

--- moodleconfig/config.php
+++ moodleconfig/config.php
@@ -37,26 +37,29 @@ $CFG->dbhost    = 'postgres';
 // End postgres
 
 
-//$CFG->wwwroot   = 'http://192.168.1.139';
-$CFG->wwwroot   = 'http://localhost';
+//$CFG->wwwroot   = 'http://localhost';
+$CFG->wwwroot   = 'http://192.168.1.127';
 $CFG->dataroot  = '/var/www/moodledata';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 02777;
 
-define('MDL_PERF', true);
-define('MDL_PERFDB', true);
-define('MDL_PERFTOLOG', true);
-define('MDL_PERFTOFOOT', true);
+//define('MDL_PERF', false);
+//define('MDL_PERFDB', false);
+//define('MDL_PERFTOLOG', false);
+//define('MDL_PERFTOFOOT', false);
+
+// H5P Specific
+$CFG->mod_hvp_backup_libraries = '0';
 
 //=========================================================================
 // 7. SETTINGS FOR DEVELOPMENT SERVERS - not intended for production use!!!
 //=========================================================================
 //
 // Force a debugging mode regardless the settings in the site administration
-@error_reporting(E_ALL | E_STRICT);   // NOT FOR PRODUCTION SERVERS!
+@error_reporting(E_ALL);   // NOT FOR PRODUCTION SERVERS!
 @ini_set('display_errors', '1');         // NOT FOR PRODUCTION SERVERS!
-$CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
+$CFG->debug = (E_ALL);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
 $CFG->debugdisplay = 1;              // NOT FOR PRODUCTION SERVERS!
 //
 // You can specify a comma separated list of user ids that that always see
---
+++ scripts/copy_xdebugprofiles.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker cp 7312f484ffb4:/xdebugprofiles ./xdebugprofiles/
